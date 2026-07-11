2c3867f6261f5dfded21c55d8a52a0a1ce06475b
Eduardo Goes <edugoes215@gmail.com>
2026-07-11 18:02:49 -0300

    Validates user access

--- public/components/header.js
+++ public/components/header.js
@@ -1,5 +1,25 @@
 class Header extends HTMLElement {
     connectedCallback() {
+        const username = localStorage.getItem('name');
+        const isLogged = username !== null;
+
+        const authMenu = isLogged 
+            ? `
+                <li class="nav-items">
+                    <!-- Podemos até mostrar o nome da pessoa no menu! -->
+                    <a href="/pages/profile.html">Ver Perfil</a>
+                </li>
+                <li class="nav-items">
+                    <a href="#" id="btn-sair">Sair</a>
+                </li>
+              ` 
+            : `
+                <li class="nav-items">
+                    <a href="/pages/login.html">Login</a>
+                </li>
+              `
+        ;
+
         this.innerHTML = `
             <header>
                 <nav>
@@ -11,10 +31,25 @@ class Header extends HTMLElement {
                         <li class="nav-items">
                             <a href="/pages/open-ticket.html">Abrir Chamado</a>
                         </li>
+                        ${authMenu}
                     </ul>
                 </nav>
             </header>
-        `
+        `;
+
+        if (isLogged) {
+            const btnSair = this.querySelector('#btn-sair');
+            btnSair.addEventListener('click', (event) => {
+                event.preventDefault();
+                
+                localStorage.removeItem('id');
+                localStorage.removeItem('name');
+                localStorage.removeItem('email');
+                localStorage.removeItem('role');
+                
+                window.location.href = '/pages/login.html';
+            });
+        }
     }
 }
 
